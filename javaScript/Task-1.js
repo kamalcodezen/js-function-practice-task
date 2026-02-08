eaf1d8a62dc1d8745e7f45384b60e302dff498de
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-09 02:01:12 +0530

    task 1 complete

---
+++ javaScript/Task-1.js
@@ -0,0 +1,18 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiplyFourNumber(a, b, c, d) {
+    let mul = a * b * c * d;
+    return mul;
+}
+
+let result = multiplyFourNumber(2, 4, 5, 6);
+console.log(result);
+
+console.log(multiplyFourNumber(4, 7, 3, 7));
+
+let nums1 = 6;
+let nums2 = 3;
+let nums3 = 9;
+let nums4 = 4;
+
+console.log(multiplyFourNumber(nums1, nums2, nums3, nums4));
